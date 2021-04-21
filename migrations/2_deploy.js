cbb4dbca27e7d03da2e862f972c3cee74cd6d47a
Brandon Torres <torresbm95@gmail.com>
2021-04-21 16:01:13 -0400

    Establish Token Contract and migration

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -3,6 +3,7 @@ const dBank = artifacts.require("dBank");
 
 module.exports = async function(deployer) {
 	//deploy Token
+	await deployer.deploy(Token); 
 
 	//assign token into variable to get it's address
 	
